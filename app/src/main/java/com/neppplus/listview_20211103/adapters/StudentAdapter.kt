4215cc3889f5d36435adb0426f8957e18ad84800
김준기 <gugi329@daum.net>
2021-11-04 00:37:37 +0900

    학생들을 뿌려주는 Adapter 기초 작업중  - ArrayAdapter 기능 활용 => 상속  - 부모에게 넘겨줄 재료 3개도 같이 받아서 만들도록.

---
+++ app/src/main/java/com/neppplus/listview_20211103/adapters/StudentAdapter.kt
@@ -0,0 +1,11 @@
+package com.neppplus.listview_20211103.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.neppplus.listview_20211103.datas.StudentData
+
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>(  ) {
+}
