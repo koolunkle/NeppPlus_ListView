5c8bfa5ffa8749b63da4d119d083584a84495764
김준기 <gugi329@daum.net>
2021-11-04 00:42:00 +0900

    안전해진 tempRow를 row에 옮겨담자. 결과로 최종 지정

--- app/src/main/java/com/neppplus/listview_20211103/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20211103/adapters/StudentAdapter.kt
@@ -22,6 +22,13 @@ class StudentAdapter(
             tempRow = mInflater.inflate(R.layout.student_list_item, null)
         }
 
+//        tempRow가 비어있다면 => 채워줌.
+//        tempRow가 비었을 가능성은? 없다
+//        안전하게 사용가능한 row를 만들자.
+        val row = tempRow!!
+
+        return row
+
     }
 
 }
