8973cc891546cf820b0ba0f50aaf8ab68dd96b52
김준기 <gugi329@daum.net>
2021-11-04 00:52:17 +0900

    리스트뷰의 이벤트 처리  - setOn"ItemClick"Listener  - position 변수 / ArrayList 변수 활용 로직 작성

--- app/src/main/java/com/neppplus/listview_20211103/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20211103/MainActivity.kt
@@ -2,6 +2,8 @@ package com.neppplus.listview_20211103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.neppplus.listview_20211103.adapters.StudentAdapter
 import com.neppplus.listview_20211103.datas.StudentData
 import kotlinx.android.synthetic.main.activity_main.*
@@ -26,6 +28,21 @@ class MainActivity : AppCompatActivity() {
 
         mStudentAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
         studentListView.adapter = mStudentAdapter
+
+        //        리스트뷰의 각 줄이 눌린 이벤트 처리
+        studentListView.setOnItemClickListener { adapterView, view, position, l ->
+
+//            position : 몇번 줄이 눌렸는가? 알려주는 역할.
+            Log.d("리스트뷰클릭", "${position}번 줄 클릭됨")
+
+//            눌린 위치에 맞는 학생 데이터 변수에 저장.
+            val clickedStudent = mStudentList[position]
+
+//            ex. 눌린 학생의 이름을 토스트로 출력
+            Toast.makeText(this, "${clickedStudent.name} 학생 클릭됨", Toast.LENGTH_SHORT).show()
+
+
+        }
 
     }
 }
