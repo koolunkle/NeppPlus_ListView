20c023f645856620e1536468bc564d8925e908ee
김준기 <gugi329@daum.net>
2021-11-04 00:40:28 +0900

    convertView를 tempRow에 옮겨 담아서, 비어있는 상태라면 채워주기

--- app/src/main/java/com/neppplus/listview_20211103/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20211103/adapters/StudentAdapter.kt
@@ -1,11 +1,27 @@
 package com.neppplus.listview_20211103.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.listview_20211103.R
 import com.neppplus.listview_20211103.datas.StudentData
 
 class StudentAdapter(
     val mContext: Context,
     val resId: Int,
     val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>( mContext, resId, mList ) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.student_list_item, null)
+        }
+
+    }
+
 }
