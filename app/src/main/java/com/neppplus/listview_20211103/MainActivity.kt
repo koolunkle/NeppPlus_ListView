a39023c84a7ce774b37d4fe28c6f32ed1e1a1134
김준기 <gugi329@daum.net>
2021-11-04 00:43:16 +0900

    액티비티에서 ArrayList 생성 + StudentData들을 담아주는 작업  - 코드로 직접 타이핑 : 임시방편

--- app/src/main/java/com/neppplus/listview_20211103/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20211103/MainActivity.kt
@@ -2,10 +2,24 @@ package com.neppplus.listview_20211103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listview_20211103.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<StudentData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( StudentData( "조경진", 1988, "서울시 동대문구" )  )
+        mStudentList.add( StudentData("김민성", 1998, "서울시 도봉구") )
+        mStudentList.add( StudentData("김준기", 1996, "경기도 남양주시") )
+        mStudentList.add( StudentData("방우진", 1983, "경기도 고양시") )
+        mStudentList.add( StudentData("이아현", 1996, "서울시 동대문구") )
+        mStudentList.add( StudentData("이지원", 1993, "서울시 관악구") )
+        mStudentList.add( StudentData("차수나", 1977, "서울시 성북구") )
+        mStudentList.add( StudentData("김경식", 1992, "서울시 중랑구") )
+
     }
 }
