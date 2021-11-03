dd48e45754036ac4a8f05888d586c236576ccb15
김준기 <gugi329@daum.net>
2021-11-04 00:48:37 +0900

    실제 데이터 뿌리기 준비작업  - 근거 데이터 : mList [position]  - 뿌려줄 UI 태그 : row.findViewById

--- app/src/main/java/com/neppplus/listview_20211103/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20211103/adapters/StudentAdapter.kt
@@ -1,10 +1,12 @@
 package com.neppplus.listview_20211103.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppplus.listview_20211103.R
 import com.neppplus.listview_20211103.datas.StudentData
 
@@ -27,6 +29,18 @@ class StudentAdapter(
 //        안전하게 사용가능한 row를 만들자.
         val row = tempRow!!
 
+//        기본 모양으로 만들어진 row를 이용해서 -> 실제 데이터를 적용시키고 나서
+
+//        목록으로 들어온 mList중 position에 맞는 학생데이터 추출
+//        cf : ArrayList => 0번칸 ~ 갯수-1번칸 까지 존재함.
+        val data = mList[ position ]
+
+//        student_list_item에 적어둔 id로 => 코틀린에서 다루도록 처리.
+        val txtName = row.findViewById<TextView>(R.id.txtName)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+        val txtAddress = row.findViewById<TextView>(R.id.txtAddress)
+
+//        최종 결과로 나가자.
         return row
 
     }
